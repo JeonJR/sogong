5a2db69c58443e1087f1be5312af6be9819e086c
JeonJR <j161021@gmail.com>
2016-06-16 16:08:53 +0900

    Create clientManagement.cpp

---
+++ clientManagement.cpp
@@ -0,0 +1,55 @@
+#include "clentManagement.h"
+
+void ClientCollection::signUp(){
+	
+	string str = "";
+	int newMember = 0;
+	for (; n[newMember] != 0; newMember++);
+
+	//while (str == "") {
+		cout << "name 입력 : ";
+		cin >> str;
+	//	if (str == "\n") { cout << "name 을 입력하세요"; }
+	//	else
+		totalClient[newMember].MakeName(str);
+	//}
+	
+	
+	//cout << "praivateNum 입력 : ";
+	//cin >> str;
+	//totalClient[newMember].MakePN(str);
+
+	cout << "address 입력 : ";
+	cin >> str;
+	totalClient[newMember].MakeADD(str);
+
+	cout << "email 입력 : ";
+	cin >> str;
+	totalClient[newMember].MakeEMAIL(str);
+
+	cout << "ID 입력 : ";
+	cin >> str;
+	totalClient[newMember].MakeID(str);
+
+	cout << "PW 입력 : ";
+	cin >> str;
+	totalClient[newMember].MakePW(str);
+
+	n[newMember] = true;
+
+}
+
+void Client::MakeName(string str) 
+{	
+	name = str; }
+//void MakePN(string str) { praivateNum = str; }
+void Client::MakeADD(string str) 
+{ address = str; }
+void Client::MakeEMAIL(string str)
+{ email = str; }
+void Client::MakeID(string str)
+{ ID = str; }
+void Client::MakePW(string str)
+{ PW = str; }
+
+}
